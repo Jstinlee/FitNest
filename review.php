f27257f70eda6ebd601762997fc55d0943f1aa83
yeongshengnii <32522146+yeongshengnii@users.noreply.github.com>
2017-11-12 02:30:17 +0800

    Fix some bug
    Adding ";" in the end of line 14 and 21

--- review.php
+++ review.php
@@ -11,14 +11,14 @@ if (isset($_POST['reviewT'])) {
 if(isset($_POST['reviewed'])){
   $rating = $_POST['star'];
   $comment = $_POST['comment'];
-  $id = $_SESSION['id']
+  $id = $_SESSION['id'];
   $reviewer = $_SESSION['username'];
   $sql = "SELECT users.userName FROM users,trainingSession
           WHERE users.username = trainingSession.username
           AND sessionID = $id";
   $result = mysqli_query($mysqli, $sql);
   $row = mysqli_fetch_assoc($result);
-  $reviewed =  $row['userName']
+  $reviewed =  $row['userName'];
 
   $sql2 =  "INSERT INTO review(reviewerUsername,reviewedUsername,rating,comment,sessionID)
             VALUES ('$reviewer','$reviewed',$rating,'$comment',$id)";
